e41309c170434bcbc2c1a6c2e622e612dd8c6fa0
Raufu Abdulraman <coder0214h@gmail.com>
2026-03-05 22:26:35 +0100

    static roots

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+def health():
+    return {"status": "ok"}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/', include('app.urls')),
+    path('health/', health, name='health')
 ]
